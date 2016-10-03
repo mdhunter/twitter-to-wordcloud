a4bd75644e1255e3101fa696c39ad6d0d2315931
Mat Hunter <mathew.hunter@gmail.com>
2016-10-03 19:12:09 -0400

    Created twitter_to_wordcloud.py
    Created a wrapper twitter_to_wordcloud.py script to orchestrate the other scripts in generating a wordcloud image from the Twitter archive.

---
+++ twitter_to_wordcloud.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Reads one or more Twitter archives, building and filtering a corpus, and then
+#  generating a wordcloud image.
+#
+# Copyright 2016 Mathew Hunter
+
+import twitter_to_corpus
+import filter_corpus
+import corpus_to_wordcloud
+
+import argparse
+import os
+import PIL
+
+
+if __name__ == "__main__":
+
+    # Create an argument parser and parse the args
+    parser = argparse.ArgumentParser(description="Reads the Twitter archive from the specified files, building a wordcloud")
+    parser.add_argument("source_file", nargs="+", help="the source file(s) to process")
+    args = parser.parse_args()
+
+    # Produce a corpus
+    corpus = twitter_to_corpus.generate_corpus(args.source_file)
+
+    # Filter it
+    filtered_corpus = filter_corpus.filter_corpus_words(corpus)
+
+    # Generate a wordcloud image
+    image = corpus_to_wordcloud.generate_image_from_words(filtered_corpus)
+
+    # Output the file
+    output_filename = os.path.dirname(os.path.realpath(__file__)) + "test.png"
+    image.to_file(output_filename)
+
+    # Show the image
+    image = PIL.Image.open(output_filename)
+    image.show()
